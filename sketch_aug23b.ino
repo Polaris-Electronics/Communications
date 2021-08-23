373c3a3549d7177ef6a7596c5c4be0c37dbf56a9
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2021-08-23 15:08:09 -0600

    Add files via upload

---
+++ sketch_aug23b.ino
@@ -0,0 +1,56 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+//Declaremos los pines CE y el CSN
+#define CE_PIN 9
+#define CSN_PIN 10
+ 
+//Variable con la dirección del canal que se va a leer
+byte direccion[5] ={'c','a','n','a','l'}; 
+
+//creamos el objeto radio (NRF24L01)
+ RF24 radio(CSN_PIN, CE_PIN);
+
+//vector para los datos recibidos
+float datos[3];
+
+void setup()
+{
+ //inicializamos el NRF24L01 
+  radio.begin();
+  //inicializamos el puerto serie
+  Serial.begin(9600); 
+  
+  //Abrimos el canal de Lectura
+  radio.openReadingPipe(1,direccion);
+  
+    //empezamos a escuchar por el canal
+  radio.startListening();
+ 
+}
+ 
+void loop() {
+ uint8_t numero_canal;
+ //if ( radio.available(&numero_canal) )
+ if ( radio.available() )
+ {    
+     //Leemos los datos y los guardamos en la variable datos[]
+     radio.read(datos,sizeof(datos));
+     
+     //reportamos por el puerto serial los datos recibidos
+     Serial.print("Dato0= " );
+     Serial.print(datos[0]);
+     Serial.print(" V, ");
+     Serial.print("Dato1= " );
+     Serial.print(datos[1]);
+     Serial.print(" ms, ");
+     Serial.print("Dato2= " );
+     Serial.println(datos[2]);
+ }
+ else
+ {
+     Serial.println("No hay datos de radio disponibles");
+ }
+ delay(1000);
+}
